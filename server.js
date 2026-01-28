a5036b23de24d8572fb849ac1f36a16b5c493735
Pieter Allen <pieter.allen@binus.ac.id>
2026-01-28 22:51:39 +0700

    Add logs to monitor which container serves

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+const replicaApp = process.env.APP_NAME; 
+
 app.use("/images", express.static(path.join(__dirname, "images")));
+
 
 app.use("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
-  console.log(`request served by node app`);
+  // liat instance app mana yang lagi layanin request
+  console.log(`request served by ${replicaApp}`);
 });
 
 app.listen(port, () => {
-  console.log(`node app is listening on port ${port}`);
+  console.log(`${replicaApp} is listening on port ${port}`);
 });
